abc65d3ace59c888a9c133310a00cce28e24a643
hyearin <hyearin5628@naver.com>
2022-01-10 17:48:48 +0900

    first commit

---
+++ js/script.js
@@ -0,0 +1,37 @@
+$(document).ready(function($) {
+
+    $(".menu>ul>li>.scroll_move").click(function(event){         
+
+            event.preventDefault();
+
+            $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
+
+    });
+
+var slideIndex = 0;
+
+setInterval(function () {
+
+    if(slideIndex > 2){
+        slideIndex = 0;
+    };
+    $(".slide_box_wrap>.slide_box").css({"top" : -800 * slideIndex });
+
+    console.log(slideIndex);
+
+
+    slideIndex++;
+
+},5000);
+
+
+
+
+
+}); /*레디끝 */
+
+
+
+
+
+
